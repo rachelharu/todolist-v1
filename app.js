1d916a8dc96fdeb265393f5e574cceea84a4b917
rachelharu <rachel.haru9@gmail.com>
2021-11-03 15:07:25 -0700

    app now adds user input to list

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const bodyParser = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", (req, res) => {
   let today = new Date();
@@ -12,15 +19,22 @@ app.get("/", (req, res) => {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
-     };
+  };
 
 
   let day = today.toLocaleDateString("en-US", options);
 
   //renders file called list, then looks for variable name kindOfDay and replaces it with value of day
-  res.render('list',{kindOfDay: day})
+  res.render('list', {kindOfDay: day, newListItems: items})
 });
 
+
+//catches the post request of button click
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 
 
 app.listen(3000, () => {

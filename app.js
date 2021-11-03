8253a94dd04ac746d7e2322713a495e53f7b2030
rachelharu <rachel.haru9@gmail.com>
2021-11-03 14:31:52 -0700

    change switch, create object options and variable day to get date instead

--- app.js
+++ app.js
@@ -7,41 +7,18 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   let today = new Date();
-  const currentDay = today.getDay();
-  let day = "";
 
-  //checks if what day of the week it is
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error");
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+     };
 
-  }
 
-  //rendering file called list, then looks for variable name and replaces it
-  res.render('list', {
-    kindOfDay: day
-  })
+  let day = today.toLocaleDateString("en-US", options);
+
+  //renders file called list, then looks for variable name kindOfDay and replaces it with value of day
+  res.render('list',{kindOfDay: day})
 });
 
 
